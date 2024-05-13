6fa26fde537baec53885e1a95cedff523d8539a1
unknown <edvan.s.pereira@aluno.senai.br>
2024-05-13 20:26:10 -0300

    criando o evento click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 let lsEmoticons = ['😊','😁','😍','🤐','😥','😴','😎','🤔' ]
 
+
+// replicando os itens na tebela
 const tamanho = lsEmoticons.length;
 for (let i = 0; i < tamanho; i++) {
     lsEmoticons.push(lsEmoticons[i])
     
+}
+
+// para cada bloco incluir o evento de click
+let isBloco = document.getElementsByClassName('bloco');
+for (const b of isBloco) {
+    b.addEventListener('click', () => {
+        mostrar(b)
+    });
+    
+}
+
+function mostrar(b){
+ b.innerHTML = '😎'
 }
