c2db51848b29a94944b56a45c51b65013d3ecc00
unknown <edvan.s.pereira@aluno.senai.br>
2024-05-13 21:38:21 -0300

    add a função escolha correta

--- js/main.js
+++ js/main.js
@@ -26,9 +26,36 @@ for (const b of isBloco) {
     id++;
 }
 
+//variavel global
+let aberto = ['', '']
+
 function mostrar(b){
- b.innerHTML =  blocos[b.id];
- 
+
+if(b.innerHTML != '') return;
+
+if(aberto[0] != '' && aberto[1] != "") testar();
+
+  if(aberto[0] == ''){
+    aberto[0] = b;
+    b.innerHTML = blocos[b.id];
+    return;
+
+   }
+  if(aberto[1] == ''){
+    aberto[1] = b;
+    b.innerHTML = blocos[b.id];
+    return;
+   }
+
+}
+function testar(){
+
+    if(aberto[0].innerHTML !=  aberto[1].innerHTML){
+        aberto[0].innerHTML = "";
+        aberto[1].innerHTML = "";
+     } 
+     aberto[0] = '';
+     aberto[1] = '';
 }
 function sortear(){
  return Math.floor(Math.random() * lsEmoticons.length);
