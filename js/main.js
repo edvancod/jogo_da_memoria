706b1eebd2a5d8a6af816aa5b978bbdf3915c2e7
unknown <edvan.s.pereira@aluno.senai.br>
2024-05-13 21:10:39 -0300

    fazendo a seleção aleatoria

--- js/main.js
+++ js/main.js
@@ -9,14 +9,28 @@ for (let i = 0; i < tamanho; i++) {
 }
 
 // para cada bloco incluir o evento de click
+let blocos = {
+
+};
+
 let isBloco = document.getElementsByClassName('bloco');
+let id = 0;
+
 for (const b of isBloco) {
     b.addEventListener('click', () => {
         mostrar(b)
     });
-    
+    icon = lsEmoticons.splice(sortear(),1); // aramazenar o icon
+    blocos[`id${id}`] = icon;
+    b.id = `id${id}`;
+    id++;
 }
 
 function mostrar(b){
- b.innerHTML = '😎'
+ b.innerHTML =  blocos[b.id];
+ 
+}
+function sortear(){
+ return Math.floor(Math.random() * lsEmoticons.length);
+ 
 }
